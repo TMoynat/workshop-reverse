b1b68da13a3d6ece509886b09b9ec6426b575a84
Thibault Moynat <thibault.moynat@epitech.eu>
2020-03-20 12:46:08 +0100

    feat: create random exercise

--- asm_reader.c
+++ asm_reader.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int absolutlyNotUsefull()
+int youDidItWellM()
 {
-    int a = 8;
+    int a = 4;
 	return (a * 2);
 }
 
 int main (void)
 {
     printf("The flag should be like PoC{foo42}\n\tfoo:  function called\n\t42:  program output\n");
-	return absolutlyNotUsefull();
+	return youDidItWellM();
 }
---
+++ random.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int compare(char *s1, char *s2)
+{
+    for (int i = 0; i < 50; i += 1) {
+        if (s1[i] != s2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int ac, char **av)
+{
+    const char charset[] = "IHaveNoIdeaWtfImDoingRightNow";
+    const int len = strlen(charset);
+    char res[50] = {};
+
+    if (ac != 2) {
+        printf("Gimme pass bro!\n");
+        return (-1);
+    }
+    if (strlen(av[1]) != 50) {
+        printf("Not even correct length!\n");
+        return (-1);
+    }
+    for (int i = 0; i < 50; i++) {
+        res[i] = charset[((i % strlen(strchr(charset, (charset[i % (len)])))) + i) % len];
+    }
+    if (compare(av[1], res)) {
+        printf("PoC{%s}\n", av[1]);
+        return 0;
+    }
+    dprintf(2, "N0pe!\n");
+    return 1;
+}
